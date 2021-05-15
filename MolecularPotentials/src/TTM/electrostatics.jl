b59b04f51db742d8c6c5f5516d48c3866de63b01
Joe Heindel <heindelj@uw.edu>
2021-05-14 18:06:19 -0700

    ij_bonded

--- MolecularPotentials/src/TTM/electrostatics.jl
+++ MolecularPotentials/src/TTM/electrostatics.jl
@@ -4,11 +4,11 @@ include("smear.jl")
 
 mutable struct Electrostatics
     natom::Int
-    q::MVector{natom, Float64}
-    damping_fac::MVector{natom, Float64}
+    q::Vector{Float64} # natom
+    damping_fac::Vector{Float64} # natom
     α::Vector{MVector{3, Float64}}
     ϕ::Vector{MVector{3, Float64}}
-    E_field_q::MVector{natom, Float64}
+    E_field_q::Vector{Float64} # natom
     E_field_dip::Vector{MVector{3, Float64}}
     dip_dip_tensor::Vector{MVector{3, Float64}}
     
@@ -26,11 +26,12 @@ function ij_bonded(i::Int, j::Int)
     """
     Determine if i and j are a part of the same water.
     Since everything has to be in OHH order, we can just infer from the indices.
+    Note that this won't check if your input is valid (all indices should be >= 1).
     """
-    (i-1) ÷ 3 == (j-1) ÷ 3
+    return (i-1) ÷ 3 == (j-1) ÷ 3
 end
 
-function electrostatics(coords::SMatrix{3, 3, Float64}, grads::Union{MMatrix{3, 3, Float64}, Nothing}=nothing, elec_data::Electrostatics)
+function electrostatics( elec_data::Electrostatics, coords::SMatrix{3, 3, Float64}, grads::Union{MMatrix{3, 3, Float64}, Nothing}=nothing)
     elec_data.α_sqrt = get_α_sqrt(elec_data.α)
 
     for i in 1:(elec_data.natom-1)
