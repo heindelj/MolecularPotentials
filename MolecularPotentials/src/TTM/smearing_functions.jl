2e31296cdc13a5f4ad5e04c4e0085d61f0bc6b05
Joe Heindel <heindelj@uw.edu>
2021-05-15 20:50:57 -0700

    TTM4 Smearing functions

--- MolecularPotentials/src/TTM/smearing_functions.jl
+++ MolecularPotentials/src/TTM/smearing_functions.jl
@@ -66,4 +66,67 @@ function smear3_ttm3(drsq::Float64, α_12::Float64, a::Float64)
     return ts1, ts2, ts3
 end
 
-# smear for ttm4
+function smear1_ttm4(drsq::Float64, α_12::Float64, a::Float64)
+    dd::Float64    = sqrt(drsq)
+    dri::Float64   = 1.0/dd
+    drsqi::Float64 = dri^2
+
+    if (α_12 > eps(Float64))
+        g34::Float64   = exp(gammln(3.0/4.0))
+        AA::Float64    = α_12^(1.0/6.0)
+        rA::Float64    = dd/AA
+        rA4::Float64   = rA^4
+        exp1::Float64  = exp(-a*rA4)
+        a_mrt::Float64 = a^(1.0/4.0)
+
+        ts0 = (1.0 - exp1 + a_mrt*rA*g34*ttm::gammq(3.0/4.0, a*rA4))*dri
+        ts1 = (1.0 - exp1)*dri*drsqi
+        return ts0, ts1
+    end
+    ts0 = dri
+    ts1 = dri*drsqi
+    return ts0, ts1
+end
+
+function smear2_ttm4(drsq::Float64, α_12::Float64, a::Float64)
+    dd::Float64    = sqrt(drsq)
+    dri::Float64   = 1.0/dd
+    drsqi::Float64 = dri^2
+
+    if (α_12 > eps(Float64)) 
+        AA::Float64   = α_12^(1.0/6.0)
+        rA::Float64   = dd/AA
+        rA4::Float64  = rA^4
+        exp1::Float64 = exp(-a*rA4)
+
+        ts1 = (1.0 - exp1)*dri*drsqi
+        ts2 = (ts1 - (4.0/3.0)*a*exp1*rA4*dri*drsqi)*drsqi
+        return ts1, ts2
+    end
+    ts1 = dri*drsqi
+    ts2 = ts1*drsqi
+    return ts1, ts2
+end
+
+function smear3_ttm4(drsq::Float64, α_12::Float64, a::Float64)
+    dd::Float64    = sqrt(drsq)
+    dri::Float64   = 1.0/dd
+    drsqi::Float64 = dri^2
+
+    if (α_12 > eps(Float64))
+        AA::Float64   = α_12^(1.0/6.0)
+        AA4::Float64  = AA^4
+        rA::Float64   = dd/AA
+        rA4::Float64  = rA^4
+        exp1::Float64 = exp(-a*rA4)
+
+        ts1 = (1.0 - exp1)*dri*drsqi
+        ts2 = (ts1 - (4.0/3.0)*a*exp1*rA4*dri*drsqi)*drsqi
+        ts3 = (ts2 - (4.0/15.0)*dri*a*(4*a*rA4 - 1.0)*exp1/AA4)*drsqi
+        return ts1, ts2, ts3
+    end
+    ts1 = dri*drsqi
+    ts2 = ts1*drsqi
+    ts3 = ts2*drsqi
+    return ts1, ts2, ts3
+}
