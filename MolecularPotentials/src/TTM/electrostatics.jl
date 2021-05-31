7943d71a26355405f47ca0595d443026b3c915bc
Joe Heindel <heindelj@uw.edu>
2021-05-30 22:34:30 -0700

    Most of way through electrostatics

--- MolecularPotentials/src/TTM/electrostatics.jl
+++ MolecularPotentials/src/TTM/electrostatics.jl
@@ -11,7 +11,7 @@ mutable struct Electrostatics
     α::Vector{Float64} # 4 * nW
     ϕ::Vector{Float64} # natom ?
     E_field_q::Vector{MVector{3, Float64}} # 3 * natom elements
-    E_field_dip::Vector{MVector{3, Float64}} # 3 * natom elements
+    E_field_dip::Vector{Float64} # 3 * natom elements
     dip_dip_tensor::Vector{Float64} # natom * natom * 9 tensor 
     smear::Smear # smearing method
     dipoles_converged::Bool # convergence option
@@ -20,13 +20,13 @@ end
 Electrostatics(charges::Vector{Float64}, C::TTM_Constants) =
 Electrostatics(length(charges), charges, zeros(3 * length(charges)), repeat([C.damping_factor_O, C.damping_factor_H, C.damping_factor_H, C.damping_factor_M], length(charges)),
 repeat([C.α_O, C.α_H, C.α_H, C.α_M], length(charges)), zeros(length(charges)), [@MVector zeros(3) for _ in 1:length(charges)],
-[@MVector zeros(3) for _ in 1:length(charges)], zeros(3 * 3 * length(charges) * length(charges)), get_smear_type(C.name), false)
+zeros(3 * length(charges)), zeros(3 * 3 * length(charges) * length(charges)), get_smear_type(C.name), false)
 
 function get_α_sqrt(α::Vector{Float64})
     α_sqrt = zeros(4 * length(α))
     for i in 1:length(α)
-        for j in 1:4
-            α_sqrt[4*(i-1)+j] = sqrt(α[i])
+        for j in 1:3
+            α_sqrt[3*(i-1)+j] = sqrt(α[i])
         end
     end
     return α_sqrt
@@ -46,32 +46,29 @@ function electrostatics(elec_data::Electrostatics, coords::Matrix{Float64}, grad
 
     # should be possible to wrap in @distributed or @Threads.threads()
     for i in 1:(elec_data.natom-1)
-        i3::Int = 3 * (i - 1)
+        i3::Int = 3 * (i - 1) + 1
         for j in (i+1):elec_data.natom
-            j3::Int = 3 * (j - 1)
+            j3::Int = 3 * (j - 1) + 1
             @views r_ij::Vector{Float64} = coords[:, i] - coords[:, j]
             r_ij_sq::Float64 = r_ij ⋅ r_ij
 
             # charge-charge
             if !ij_bonded(i, j)
                 ts0, ts1 = elec_data.smear.smear1(r_ij_sq, elec_data.damping_fac[i] * elec_data.damping_fac[j], elec_data.smear.aCC)
-
+                
                 elec_data.ϕ[i] += ts0 * elec_data.q[j]
                 elec_data.ϕ[j] += ts0 * elec_data.q[i]
-
+                
                 for k in 1:3
                     elec_data.E_field_q[i][k] += ts1 * elec_data.q[j] * r_ij[k]
                     elec_data.E_field_q[j][k] -= ts1 * elec_data.q[i] * r_ij[k]
                 end
             end
 
-            println(elec_data.E_field_q[j][:])
-
             # dipole-dipole tensor
             aDD::Float64 = ij_bonded(i, j) ? elec_data.smear.aDD_intramolecular : elec_data.smear.aDD_intermolecular
 
             ts1, ts2 = elec_data.smear.smear2(r_ij_sq, elec_data.damping_fac[i] * elec_data.damping_fac[j], aDD)
-
             dd3 = @MMatrix zeros(3, 3)
 
             dd3[1, 1] = 3.0 * ts2 * r_ij[1] * r_ij[1] - ts1
@@ -84,10 +81,10 @@ function electrostatics(elec_data::Electrostatics, coords::Matrix{Float64}, grad
             dd3[3, 1] = dd3[1, 3]
             dd3[3, 2] = dd3[2, 3]
 
-            aiaj::Float64 = α_sqrt[i] * α_sqrt[j]
-            for k in 1:3
-                for l in 1:3
-                    elec_data.dip_dip_tensor[3 * elec_data.natom * (i3 + k) + j3 + l] = -aiaj * dd3[k, l]
+            aiaj::Float64 = α_sqrt[i3] * α_sqrt[j3]
+            for k in 0:2
+                for l in 0:2
+                    elec_data.dip_dip_tensor[3 * elec_data.natom * (i3 + k - 1) + j3 + l] = -aiaj * dd3[k+1, l+1]
                 end
             end
         end
@@ -107,35 +104,39 @@ function electrostatics(elec_data::Electrostatics, coords::Matrix{Float64}, grad
 
     natom3::Int = 3 * elec_data.natom
 
-    for i in 1:natom3
-        for j in i:natom3
-            sum__::Float64 = elec_data.dip_dip_tensor[i*natom3 + j]
-            for k in 1:i
-                sum__ -= elec_data.dip_dip_tensor[i*natom3 + k] * elec_data.dip_dip_tensor[j*natom3 + k]
+    for i in 0:natom3-1
+        for j in i:natom3-1
+            sum__::Float64 = elec_data.dip_dip_tensor[i*natom3 + j + 1]
+            for k in 0:(i-1)
+                sum__ -= elec_data.dip_dip_tensor[i*natom3 + k + 1] * elec_data.dip_dip_tensor[j*natom3 + k + 1]
+                if i == j
+                end
             end
             if i == j
-                @assert sum__ >= 0.0 "Sum is not greater than zero."
-                elec_data.E_field_dip[i] = sqrt(sum__)
+                @assert sum__ > 0.0 "Sum is not greater than zero."
+                elec_data.E_field_dip[i+1] = sqrt(sum__)
             else
-                elec_data.dip_dip_tensor[j*natom3 + i] = sum__ / elec_data.E_field_dip[i]
+                elec_data.dip_dip_tensor[j*natom3 + i + 1] = sum__ / elec_data.E_field_dip[i+1]
             end
         end
     end
 
     # solve L*y = sqrt(a)*Efq storing y in dipole[]
-    for i in 1:natom3
-        sum__::Float64 = α_sqrt[i] * elec_data.E_field_q[i]
+    xyz::Int = 1
+    for i in 0:natom3-1
+        sum__::Float64 = α_sqrt[i+1] * elec_data.E_field_q[(i ÷ 3) + 1][xyz]
         for k in 1:i
-            sum__ -= elec_data.dip_dip_tensor[i*natom3 + k] * elec_data.dipole[k]
+            sum__ -= elec_data.dip_dip_tensor[i*natom3 + k] * elec_data.dipoles[k]
         end
 
-        elec_data.dipole[i] = sum__ / elec_data.E_field_dip[i]
+        elec_data.dipoles[i+1] = sum__ / elec_data.E_field_dip[i+1]
+        xyz == 3 ? xyz = 1 : xyz += 1 
     end
 
     # solve L^T*x = y
     for i in natom3:-1:1
         i1::Int = i - 1
-        sum__::Float64 = elec_data.dipole[i1]
+        sum__::Float64 = elec_data.dipoles[i1]
         for k in i:natom3
             sum__ -= elec_data.dip_dip_tensor[k*natom3 + i1] * elec_data.dipole[k]
         end
