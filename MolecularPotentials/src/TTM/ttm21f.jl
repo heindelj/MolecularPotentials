4906a659f48655787d7d00db5e8a027e20cf4126
Joe Heindel <heindelj@uw.edu>
2021-06-11 15:42:47 -0700

    Finished porting TTM. NEEDS TESTING

--- MolecularPotentials/src/TTM/ttm21f.jl
+++ MolecularPotentials/src/TTM/ttm21f.jl
@@ -30,8 +30,6 @@ function compute_M_site_crd!(ttm21f::TTM21F, coords::AbstractMatrix{Float64})
     end
 end
 
-#function charges!(ttm21f::TTM21F)
-#end
 function evaluate!(ttm21f::TTM21F, coords::AbstractMatrix{Float64}, grads::Union{Matrix{Float64}, Nothing}=nothing)
     """
     Evaluates the ttm21f energy of the system containing water called coords.
@@ -39,8 +37,8 @@ function evaluate!(ttm21f::TTM21F, coords::AbstractMatrix{Float64}, grads::Union
     compute_M_site_crd!(ttm21f, coords)
     # @SPEED factor all of the gradient intermediates into a different function or block
     # which is only executed if the gradients are actually requested
+    grads_E = zeros(3, ttm21f.num_waters * 4) # O H H M
     grads_q = convert(MArray{Tuple{3, 3, 3}, Float64, 3, 27}, zeros(27))
-    grads_E = zero(grads)
     # calculate the INTRA-molecular energy and Dipole Moment
     # Surface (Partridge-Schwenke)
 
@@ -68,7 +66,7 @@ function evaluate!(ttm21f::TTM21F, coords::AbstractMatrix{Float64}, grads::Union
         if (grads === nothing)
             continue
         end
-
+        # charge gradients
         for k in 1:3
             grads_q[1, 1, k] = dq3[1, 1, k] + tmp*(dq3[1, 1, k] + dq3[1, 2, k])
             grads_q[2, 1, k] = dq3[2, 1, k] + tmp*(dq3[2, 1, k] + dq3[2, 2, k])
@@ -92,35 +90,80 @@ function evaluate!(ttm21f::TTM21F, coords::AbstractMatrix{Float64}, grads::Union
     # Calculate the vdW interactions for all atoms                  !
     #---------------------------------------------------------------!
 
-    # PICK UP FROM HERE PORTING THINGS OVER STILL    
-
     E_vdw::Float64 = 0.0
+    # Loop through all of the M sites for van der Waal's interactions
+    for i in 1:ttm21f.num_waters-1
+        for j in i+1:ttm21f.num_waters
+            # Calculate Oxygen-Oxygen interactions
+            @view Rij = ttm21f.M_site_coords[:, i] - ttm21f.M_site_coords[:, j]
+            expon = ttm21f.constants.vdwD * exp(-ttm21f.constants.vdwE * norm(Rij))
 
-    for (size_t i = 0; i < nw - 1; ++i) {
-        for (size_t j = i + 1; j < nw; ++j) {
-            // Calculate Oxygen-Oxygen interactions
-            double dRijsq = 0.0, Rij[3];
-            for (size_t k = 0; k < 3; ++k) {
-                Rij[k] = crd[3*3*i + k] - crd[3*3*j + k];
-                dRijsq += Rij[k]*Rij[k];
-            }
+            # Add Evdw van der Waal energy for O-O interaction
+            x::Float64 = 1 / (Rij ⋅ Rij)
+            E_vdw += expon + x * x * x * (ttm21f.constants.vdwC + x * x * (ttm21f.constants.vdwB + x * ttm21f.constants.vdwA))
 
-            const double dRij = std::sqrt(dRijsq);
-            const double expon = vdwD*std::exp(-vdwE*dRij);
+            if (grads !== nothing) 
+                tmp::Float64 = (ttm21f.constants.vdwE * expon / norm(Rij) 
+                + x*x*x*x*(6 * ttm21f.constants.vdwC 
+                + x*x*(10 * ttm21f.constants.vdwB + 12 * x * ttm21f.constants.vdwA)))
 
-            // Add Evdw van der Waal energy for O-O interaction
-            const double x = 1/dRijsq;
-            Evdw += expon + x*x*x*(vdwC + x*x*(vdwB + x*vdwA));
+                # add forces to indices for the M site
+                for k in 1:3
+                    grads_E[k, 4*i] -= tmp * Rij[k]
+                    grads_E[k, 4*j] += tmp * Rij[k]
+                end
+            end
+        end
+    end
+    
+    # if no gradients then just return energy here.
+    if (grads === nothing)
+        return E_int + E_vdw + electrostatics(ttm21f.elec_data, coords, nothing)
+    end
 
-            if (grad) {
-                const double tmp = vdwE*expon/dRij
-                    + x*x*x*x*(6*vdwC + x*x*(10*vdwB + 12*x*vdwA));
+    #-------------------------------------------------------------------------!
+    # Calculate the remaining part of the derivatives                         !
+    #-------------------------------------------------------------------------!
 
-                for (size_t k = 0; k < 3; ++k) {
-                    grad[3*3*i + k] -= tmp*Rij[k];
-                    grad[3*3*j + k] += tmp*Rij[k];
-                }
-            } // grad
-        }
-    }
+    E_elec::Float64 = electrostatics(ttm21f.elec_data, coords, grads_E)
+
+    # this will be relevant once I add in the iterative approach
+    # as an alternative to cholesky factorization
+    #assert(m_electrostatics.dipoles_converged())
+
+    # add electrostatic derivatives
+    for n in 1:ttm21f.num_waters
+        # add O H H gradients from PS and electrostatics (which has M site)
+        for k in 1:3
+            @views grads[:, 3 * (n-1) + k] += grads_E[:, 4 * (n-1) + k]
+        end
+
+        # redistribute the M-site derivatives
+        @views grads[:, 3 * (n-1) + 1] += (1.0 - ttm21f.constants.γ_M) * dE[:, 4*(n-1) + 4] # O
+        @views grads[:, 3 * (n-1) + 2] +=  0.5 * ttm21f.constants.γ_M  * dE[:, 4*(n-1) + 4] # H
+        @views grads[:, 3 * (n-1) + 3] +=  0.5 * ttm21f.constants.γ_M  * dE[:, 4*(n-1) + 4] # H
+    end
+
+    # derivatives from the adjustable charges of the NASA's PES
+    for n in 1:ttm21f.num_waters
+        i_O  = 3 * (n-1) + 1
+        i_H1 = 3 * (n-1) + 2
+        i_H2 = 3 * (n-1) + 3
+
+        for k in 1:3
+            grads[k, i_H1] += grads_q[k, 1, 1]*ttm21f.elec_data.ϕ[4*n + 1]  # phi(h1)
+                            + grads_q[k, 2, 1]*ttm21f.elec_data.ϕ[4*n + 2]  # phi(h2)
+                            + grads_q[k, 3, 1]*ttm21f.elec_data.ϕ[4*n + 3]  # phi(M)
+
+            grads[k, i_H2] += grads_q[k, 1, 2]*ttm21f.elec_data.ϕ[4*n + 1]  # phi(h1)
+                            + grads_q[k, 2, 2]*ttm21f.elec_data.ϕ[4*n + 2]  # phi(h2)
+                            + grads_q[k, 3, 2]*ttm21f.elec_data.ϕ[4*n + 3]  # phi(M)
+
+            grads[k, i_O]  += grads_q[k, 1, 3]*ttm21f.elec_data.ϕ[4*n + 1]  # phi(h1)
+                            + grads_q[k, 2, 3]*ttm21f.elec_data.ϕ[4*n + 2]  # phi(h2)
+                            + grads_q[k, 3, 3]*ttm21f.elec_data.ϕ[4*n + 3]  # phi(M)
+        end
+    end
+
+    return E_int + E_vdw + E_elec
 end
